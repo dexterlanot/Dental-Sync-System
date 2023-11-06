bcaa9ae7da34063634ca484d6f05fcd4959c6068
John Dexter Lanot <21-09401@g.batstate-u.edu.ph>
2023-11-05 18:57:18 -0800

    update

--- server/index.js
+++ server/index.js
@@ -2,26 +2,21 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
-
-import authRoutes from './routes/auth.js'; // Import the auth routes
-
+import authRoutes from './mongodb/routes/auth.js';
 dotenv.config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello World' });
-});
 
-// Use the auth routes
 app.use('/auth', authRoutes);
 
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+    app.listen(3000, () => console.log('Server started on port http://localhost:3000'));
   } catch (error) {
     console.log(error);
   }
